14905ae68561005985cb41e8d8b934da045b808c
Eliecer <eliecer.sanchez47@gmail.com>
2025-01-03 15:33:48 -0400

    astro details component added

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,9 @@ export type planetsProps = {
   imgSrc: string;
   id: number;
 };
+
+export type dataAstrosProps = {
+  dataAstros: planetsProps[];
+  currentIndex: number;
+  handleCLick: (index: number) => void;
+}
