49b549f75176feab2c3ca6d6161fc80fce9211d0
Eliecer <eliecer.sanchez47@gmail.com>
2025-01-06 09:23:20 -0400

    types for generateSpaceLayer

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,42 @@
-import CoverflowSlider from "./components/CoverflowSlider"
-import styles from './App.module.css'
+import CoverflowSlider from "./components/CoverflowSlider";
+import styles from "./App.module.css";
 
-function generateSpaceLayer(){
-  const layer = []
-  const starsLength = 200;
+type spaceLayer = {
+  size: string;
+  selector: string;
+  starsLength: number;
+  duration: string;
+};
 
-  for(let i = 0; i < starsLength; i++){
+function generateSpaceLayer(options: spaceLayer) {
+  const { size, selector, starsLength, duration } = options;
+  const layer = [];
+
+  for (let i = 0; i < starsLength; i++) {
     const x = Math.floor(Math.random() * 100);
     const y = Math.floor(Math.random() * 100);
-    layer.push(`${x}vw ${y}vh 0 white`)
+    layer.push(`${x}vw ${y}vh 0 white, ${x}vw ${y + 100}vh 0 white`);
   }
 
-  const container = document.querySelector(".space-1")
-  container?.style?.setProperty("--space-layer", layer.join(","))
+  const container = document.querySelector(selector);
+  container?.style.setProperty("--space-layer", layer.join(","));
+  container?.style.setProperty("--size", size);
 }
 
-generateSpaceLayer()
-
+generateSpaceLayer({
+  size: "2px",
+  selector: ".space-1",
+  starsLength: 400,
+  duration: "5s",
+});
 
 const App = () => {
   return (
-   <div className={styles.Container}>
-     <div className='space-1'></div>
-     <CoverflowSlider />
-   </div>
-  )
-}
+    <div className={styles.Container}>
+      <div className="space-1"></div>
+      <CoverflowSlider />
+    </div>
+  );
+};
 
-export default App
+export default App;
