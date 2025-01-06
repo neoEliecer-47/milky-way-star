20edffea59ba0a29b70b77a96e2f5231253521c9
Eliecer <eliecer.sanchez47@gmail.com>
2025-01-05 23:44:51 -0400

    stars added for background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import CoverflowSlider from "./components/CoverflowSlider"
 import styles from './App.module.css'
 
+function generateSpaceLayer(){
+  const layer = []
+  const starsLength = 200;
+
+  for(let i = 0; i < starsLength; i++){
+    const x = Math.floor(Math.random() * 100);
+    const y = Math.floor(Math.random() * 100);
+    layer.push(`${x}vw ${y}vh 0 white`)
+  }
+
+  const container = document.querySelector(".space-1")
+  container?.style?.setProperty("--space-layer", layer.join(","))
+}
+
+generateSpaceLayer()
+
 
 const App = () => {
   return (
    <div className={styles.Container}>
+     <div className='space-1'></div>
      <CoverflowSlider />
    </div>
   )
