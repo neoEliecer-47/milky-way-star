173e593e380bae92a8b2222f1590a19c860ef65b
Eliecer <eliecer.sanchez47@gmail.com>
2025-01-10 23:49:41 -0400

    set first details for modal details

---
+++ src/components/ModalAstroDetails.tsx
@@ -0,0 +1,11 @@
+import styles from './ModalAstroDetails.module.css'
+
+const ModalAstroDetails = () => {
+  return (
+    <article className={styles.modalContainer}>
+        MODAL
+    </article>
+  )
+}
+
+export default ModalAstroDetails
