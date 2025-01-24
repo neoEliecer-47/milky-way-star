91efc7714b1310e266eee6a54eb07a3377a62ba4
Eliecer <eliecer.sanchez47@gmail.com>
2025-01-24 11:34:38 -0400

    skeleton added for surface image and styles

--- src/components/AstroSurfaceImage.tsx
+++ src/components/AstroSurfaceImage.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { modalImagesProps } from "../types";
 import styles from "./AstroSurfaceImage.module.css";
 import CloseIcon from "./icons/CloseIcon";
+import AstroSurfaceImageSke from "./skeletons/AstroSurfaceImageSke";
 
 const AstroSurfaceImage = ({ images, onCloseModal }: modalImagesProps) => {
+  const [isSurfaceImageLoaded, setIsSurfaceImageLoaded] = useState<boolean>(false)
   return (
     <section style={{ width: "100%" }}>
       <div className={styles.buttonContainer}>
@@ -13,11 +16,13 @@ const AstroSurfaceImage = ({ images, onCloseModal }: modalImagesProps) => {
       <div className={styles.container}>
         {images.map(({ imgSrc }, index) => (
           <figure className={styles.containerImage}>
+            {!isSurfaceImageLoaded && <AstroSurfaceImageSke />}
             <img
               src={imgSrc}
               key={index}
               alt="astro-surface"
               className={styles.image}
+              onLoad={()=> setIsSurfaceImageLoaded(true)}
             />
           </figure>
         ))}
---
+++ src/components/skeletons/AstroSurfaceImageSke.tsx
@@ -0,0 +1,11 @@
+import styles from './AstroSurfaceImageSke.module.css'
+
+const AstroSurfaceImageSke = () => {
+  return (
+    <div className={styles.surfaceImgContainer}>
+
+    </div>
+  )
+}
+
+export default AstroSurfaceImageSke
